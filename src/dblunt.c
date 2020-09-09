c3fba2f68b9a0b82e16ad3c1676a2f6fda082928
stolk <b.stolk@gmail.com>
2020-09-09 11:58:17 -0700

    Added text attributes for superscript and subscript.

--- src/dblunt.c
+++ src/dblunt.c
@@ -92,3 +92,82 @@ int dblunt_string_to_vertices
 	return trias_written;
 }
 
+
+/*!
+ * Convert an ascii string to a stream of vertex data.
+ * Returns the number of triangles(x,y,x,y,x,y) written to the buffer.
+ * If fadestart >= 0, then opacity values are written as well: (x,y,o,x,y,o,x,y,o).
+ */
+int dblunt_attributed_string_to_vertices
+(
+	const char* str,		//!< characters to render.
+	const char* att,		//!< character attributes.
+	float* destbuf,			//!< output buffer to hold vertices.
+	int destbufsz, 			//!< size of output buffer in bytes.
+	float posx, float posy,		//!< target position for text render.
+	float sclx, float scly,		//!< scale of the text render.
+	float* __restrict__ textw,	//!< out: size of longest line.
+	float* __restrict__ texth	//!< out: height of text.
+)
+{
+	int trias_written = 0;
+	float* buf_writer = destbuf;
+	const int floats_per_vertex = 2;
+	const int tri_capacity = destbufsz / ( 3 * floats_per_vertex * sizeof(float) );
+	sclx *= 0.2f;	// The 'M' glyph has size 4x5, so we compensate.
+	scly *= 0.2f;
+	const int l  = (int) strlen(str);
+	const int la = att ? (int) strlen(att) : 0;
+	assert( la==0 || la==l );
+	const float linepitch = 1.25f * 5.0f * scly;
+	const float linespacing = 0.25f * 5.0f * scly;
+	float x = posx;
+	float y = posy - 5.0f*scly;
+	float maxx = x;
+	int numlines = 1;
+	int numprinted = 0;
+	for ( int charnr=0; charnr<l; ++charnr )
+	{
+		int c = str[ charnr ];
+		int a = att ? att[ charnr ] : 0;
+		const float csclx = ( a=='^' || a=='_' ) ? 0.6f * sclx : sclx;
+		const float cscly = ( a=='^' || a=='_' ) ? 0.6f * scly : scly;
+		const float coffy = ( a=='^' ) ? 0.5f * scly : ( a=='_' ) ? -0.5f * scly : 0.0f;
+		if ( c == '\n' )	// carriage return.
+		{
+			y -= linepitch;
+			x = posx;
+			numlines += 1;
+			continue;
+		}
+		if  ( c == ' ' )
+		{
+			x += 3.0f * sclx;
+			continue;
+		}
+		if ( c > 127 )
+			continue;	// only render ascii 0 to 127
+		const float width = widths[ c ];
+		const int   sz    = sizes [ c ];
+		const int   voffs = vdataoffsets[ c ];
+		const int   trias = sz/3;
+		if ( x + width*csclx > maxx ) maxx = x + width*csclx;
+		if ( trias_written + trias <= tri_capacity )
+		{
+			for ( int vnr=0; vnr<sz; ++vnr )
+			{
+				const float vx = vdata[ voffs+vnr ][ 0 ];
+				const float vy = vdata[ voffs+vnr ][ 1 ];
+				*buf_writer++ = x + vx * csclx;
+				*buf_writer++ = y + vy * cscly + coffy;
+			}
+			x += ( width + 1.0f ) * csclx;	// advance to next pos
+			trias_written += trias;
+		}
+		numprinted++;
+	}
+	*texth = numlines * linepitch - 1 * linespacing;
+	*textw = maxx - posx;
+	return trias_written;
+}
+
--- src/dblunt.h
+++ src/dblunt.h
@@ -1,6 +1,6 @@
 // dblunt.c
 // Dutch-Blunt low poly font system.
-// (c)2015 Abraham Stolk
+// (c)2015-2020 Abraham Stolk
 
 
 extern int dblunt_string_to_vertices
@@ -11,6 +11,19 @@ extern int dblunt_string_to_vertices
 	float posx, float posy,		//!< target position for text render.
 	float sclx, float scly,		//!< scale of the text render.
 	float fadestart,		//!< if >=0, start fading out at this character number and write opactities.
+	float* __restrict__ textw,	//!< out: size of longest line.
+	float* __restrict__ texth	//!< out: height of text.
+);
+
+
+extern int dblunt_attributed_string_to_vertices
+(
+	const char* str,		//!< characters to render.
+	const char* att,		//!< character attributes, like superscript/subscript.
+	float* destbuf,			//!< output buffer to hold vertices.
+	int destbufsz, 			//!< size of output buffer in bytes.
+	float posx, float posy,		//!< target position for text render.
+	float sclx, float scly,		//!< scale of the text render.
 	float* __restrict__ textw,	//!< out: size of longest line.
 	float* __restrict__ texth	//!< out: height of text.
 );
